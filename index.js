9bf92edc4f2944aa941f1a0f7ca9e19db971a35e
Ashwin Kandel <incessantmeraki@gmail.com>
2018-03-17 17:43:41 +1000

    add merge sort

--- index.js
+++ index.js
@@ -66,3 +66,29 @@ exports.heapSort = function (arr) {
   
   return arr
 }
+
+
+exports.mergeSort = function (arr) {
+
+  return mSort(arr)
+  
+  function mSort (arr) { 
+    if (arr.length < 2) 
+      return arr
+
+    var mid = Math.floor(arr.length / 2)
+    var subLeft = mSort(arr.slice(0,mid))
+    var subRight = mSort(arr.slice(mid))
+
+    return merge(subLeft, subRight)
+  }
+
+  function merge (node1, node2) {
+    var result = []
+
+    while (node1.length > 0 && node2.length > 0) {
+      result.push (node1[0] < node2[0] ? node1.shift(): node2.shift())
+    }
+    return result.concat(node1.length ? node1 : node2)
+  }
+}
