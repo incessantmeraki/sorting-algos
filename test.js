fe0a891a639e28164d4d160aa6eaaa0de756043b
Ashwin Kandel <incessantmeraki@gmail.com>
2018-03-17 12:10:55 +1000

    Test bubble sort

---
+++ test.js
@@ -0,0 +1,27 @@
+var test = require('tape')
+var {bubbleSort} = require('./index.js')
+
+function isSorted (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i+1]) 
+      return false
+  }
+  return true
+}
+
+function createRandomList (size = 10, max=1000) {
+  var arr = []
+  for (let i = 0; i < size; i++) {
+    arr.push(parseInt(Math.random()*max , 10))
+  }
+  return arr
+}
+
+
+test('test bubble sort', function (t) {
+  for (let i = 10; i < 1000; i = i + 10) {
+    let testData = createRandomList(i)
+    t.ok(isSorted(bubbleSort(testData)))
+  }
+  t.end()
+})
